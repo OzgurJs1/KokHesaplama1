cbeac37688b9269328682d6da5b1d855a2edc093
ozzgu <ozzgu@DESKTOP-LERKFDE>
2023-01-08 12:34:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int a, b, c;
+double delta,x1,x2;
+Console.WriteLine("A değerini giriniz = ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("B değerini giriniz = ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("C değerini giriniz = ");
+c = Convert.ToInt32(Console.ReadLine());
+delta = b * b - 4 * a * c;
+if(delta == 0)
+{
+    Console.WriteLine("Kökler eşit!");
+}
+else if(delta < 0)
+{
+    Console.WriteLine("Kök yok!");
+}
+else
+{
+    x1 = ((-1) * b - Math.Sqrt(delta)) / 2 * a;
+    x2 = ((-1) * b - Math.Sqrt(delta)) / 2 * a;
+    Console.WriteLine("x1 = {0},x2 = {1}", x1, x2);
+}
